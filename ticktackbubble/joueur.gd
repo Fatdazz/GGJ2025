6c663ccbbd227e9dd09bce6793e80f7e1f906897
Taïssir <50212633+Taissirbw@users.noreply.github.com>
2025-01-25 18:34:18 +0100

    boutons pour bouger

--- ticktackbubble/joueur.gd
+++ ticktackbubble/joueur.gd
@@ -2,41 +2,44 @@ extends Node2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const VENTILO_ANGLE = PI/4
-const VENTILO_VELOCITY = -200.
+const JUMP_VELOCITY = -200.0
+var VENTILO_VELOCITY = 50.
+const VITESSES = [1., 2., 3.]
 
 @onready var bubble = $Bubble
 
+#TODO : diminuer la force avec la distance aux ventilo
 
-# Active ventilo. 1 pour droite, -1 à gauche
-# renvoie le vecteur de déplacement
-func wind(direction):
-	var vec = Vector2(cos(VENTILO_ANGLE), 1.) * direction * VENTILO_VELOCITY
-
+func change_vitesse(i):
+	VENTILO_VELOCITY = 50. * VITESSES[i]
+		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	
+	# Handle jump.
+	if Input.is_action_pressed("1_right"):
+		change_vitesse(0)
+		blow(1)
+	if Input.is_action_pressed("1_left"):
+		change_vitesse(0)
+		blow(-1)
+	if Input.is_action_pressed("2_right"):
+		change_vitesse(1)
+		blow(1)
+	if Input.is_action_pressed("2_left"):
+		change_vitesse(1)
+		blow(-1)
+	if Input.is_action_pressed("3_right"):
+		change_vitesse(2)
+		blow(1)
+	if Input.is_action_pressed("3_left"):
+		change_vitesse(2)
+		blow(-1)
 	if not bubble.is_on_floor():
 		bubble.velocity += bubble.get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_pressed("right"):
-		#bubble.velocity.y = JUMP_VELOCITY
-		bubble.velocity.x *= cos(VENTILO_ANGLE) * VENTILO_VELOCITY
-		bubble.velocity.y = VENTILO_VELOCITY
-	if Input.is_action_pressed("left"):
-		#bubble.velocity = JUMP_VELOCITY
-		bubble.velocity.x *= -cos(VENTILO_ANGLE) * VENTILO_VELOCITY
-		bubble.velocity.y = VENTILO_VELOCITY
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		bubble.velocity.x = direction * SPEED
-	else:
-		bubble.velocity.x = move_toward(bubble.velocity.x, 0, SPEED)
-
 	bubble.move_and_slide()
 
-
-	
+func blow(i):
+	bubble.velocity.x = i * -VENTILO_VELOCITY
+	bubble.velocity.y = -VENTILO_VELOCITY + JUMP_VELOCITY
