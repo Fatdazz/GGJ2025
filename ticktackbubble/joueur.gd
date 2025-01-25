9f52e08d7569f98beba81949063e145b2f186cf6
Taïssir <50212633+Taissirbw@users.noreply.github.com>
2025-01-25 21:57:33 +0100

    Physique + Bas

--- ticktackbubble/joueur.gd
+++ ticktackbubble/joueur.gd
@@ -1,45 +1,41 @@
 extends Node2D
 
-@export var JUMP_VELOCITY = -200.
-@export var VENTILO_VELOCITY = 50.
+@export var JUMP_VELOCITY = -25.
+@export var VENTILO_VELOCITY = 10.
 @export var VITESSES = [1., 2., 3.]
+
 @onready var bubble = $Bubble
 
+var velocity
 signal hurt
 #TODO : diminuer la force avec la distance aux ventilo
 
 func change_vitesse(i):
-	JUMP_VELOCITY = -200. * VITESSES[i]
+	velocity = JUMP_VELOCITY * (VITESSES[i]/3.)
 		
 func _physics_process(delta: float) -> void:
 
-	var vec = Vector2(0,0)
+	velocity = JUMP_VELOCITY
 	# Handle jump.
 	if Input.is_action_pressed("1_right"):
-		change_vitesse(0)
 		blow(1)
 	if Input.is_action_pressed("1_left"):
-		change_vitesse(0)
 		blow(-1)
 	if Input.is_action_pressed("2_right"):
-		change_vitesse(1)
-		blow(1)
+		blow(2)
 	if Input.is_action_pressed("2_left"):
-		change_vitesse(1)
-		blow(-1)
+		blow(-2)
 	if Input.is_action_pressed("3_right"):
-		change_vitesse(2)
-		blow(1)
+		blow(3)
 	if Input.is_action_pressed("3_left"):
-		change_vitesse(2)
-		blow(-1)
+		blow(-3)
 	if not bubble.is_on_floor():
 		bubble.velocity += bubble.get_gravity() * delta
 	bubble.move_and_slide()
 
 func blow(i):
 	bubble.velocity.x = i * -VENTILO_VELOCITY
-	bubble.velocity.y = -VENTILO_VELOCITY + JUMP_VELOCITY
+	bubble.velocity.y = -VENTILO_VELOCITY + velocity
 	
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("obstacles"):
--- ticktackbubble/obstacle.gd
+++ ticktackbubble/obstacle.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-@export var VITESSE_ROTATION = 0.
+@export var VITESSE_ROTATION = 5.
 @export var POWER = 0.
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,5 +9,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if VITESSE_ROTATION > 0.:
+		$Sprite2D.rotate((VITESSE_ROTATION*2.*PI)/10. + delta)
  # Replace with function body.
